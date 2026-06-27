a275b72d8088af017bf6980e1650a5f0340a28c4
Delta Student <neerajs1435@gmail.com>
2026-06-27 21:57:02 +0530

    passing Multipal Values

--- src/All Topics/16_UseContextHook/Parent.jsx
+++ src/All Topics/16_UseContextHook/Parent.jsx
@@ -4,7 +4,10 @@ import Child from "./Child";
 export const UserContext = createContext();
 
 const Parent = () => {
-  const name = "Neeraj Sharma";
+  //const name = "Neeraj Sharma";
+
+  //! sending multipal values
+  const user = { name: "Yahuu", role: "Admit" };
   return (
     <>
       <h1>Parent</h1>
